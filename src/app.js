efda4f5959d153c90359f3b5d499485a3c13e091
Mert <mertturkmenoglu99@gmail.com>
2021-03-20 22:42:58 +0300

    Add basic EJS template

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+app.get('/', (req, res) => {
+  const renderOptions = {
+    pageTitle: "Home",
+  };
+
+  return res.render('HomePage', renderOptions);
+})
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
